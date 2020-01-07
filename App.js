77a122e9d0182e271e39930417de61c7d2032a04
ihsan2 <nihsan727@gmail.com>
2020-01-07 23:07:35 +0700

    add navbar

--- App.js
+++ App.js
@@ -1,76 +1,44 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {Component} from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+// screens
+import BottomNavbar from './src/screens/BottomNavbar';
+import Home from './src/screens/Home';
+import Wishlist from './src/screens/Wishlist';
+import Cart from './src/screens/Cart';
+import Profile from './src/screens/Profile';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const AppNavigator = createStackNavigator(
+  {
+    BottomNavbar: {
+      screen: BottomNavbar,
+    },
+    Home: {
+      screen: Home,
+    },
+    Wishlist: {
+      screen: Wishlist,
+    },
+    Cart: {
+      screen: Cart,
+    },
+    Profile: {
+      screen: Profile,
+    },
+  },
+  {
+    headerMode: 'none',
+    navigationOptions: {
+      headerVisible: false,
+    },
+  },
+);
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const AppContainer = createAppContainer(AppNavigator);
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <View>
-        <Text>Test</Text>
-      </View>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+// wrap all component with redux Provider and the store
+export default class App extends Component {
+  render() {
+    return <AppContainer />;
+  }
+}
