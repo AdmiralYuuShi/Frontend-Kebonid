9656b4a375a3cea114ee2624d7ffc05d222c6f19
kurniawandenyy <denykurniawan146@gmail.com>
2020-01-10 16:25:55 +0700

    login api

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import LoginUser from '../components/Login';
+import HomeProduk from '../components/Home';
+import {connect} from 'react-redux';
 
-const Login = () => {
-  return <LoginUser />;
+const Login = props => {
+  return <>{props.auth.token ? <HomeProduk /> : <LoginUser />}</>;
 };
 
-export default Login;
+const mapStateToProps = state => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(Login);
--- src/screens/Start.js
+++ src/screens/Start.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import GetStart from '../components/Start.js';
+import HomeProduk from '../components/Home';
+import {connect} from 'react-redux';
 
-const Start = () => {
-  return (
-    <>
-      <GetStart />
-    </>
-  );
+const Start = props => {
+  return <>{props.auth.token ? <HomeProduk /> : <GetStart />}</>;
 };
+const mapStateToProps = state => ({
+  auth: state.auth,
+});
 
-export default Start;
+export default connect(mapStateToProps)(Start);
